4689621c9a718701e35ee8bdc17d7bfe139e16b2
mengyaocui <mengyaocui@tencent.com>
2022-07-20 21:35:07 +0800

    0717 递归

---
+++ 递归recursion.js
@@ -0,0 +1,149 @@
+/**
+ * 递归
+ * master 公式
+ */
+
+/**
+ * 题目1：求arr, l, r
+ */
+
+/**
+ * 题目2：汉诺塔问题
+ */
+function hanno(n) {
+  hannoProcess(0, n, 'from', 'to', 'other');
+}
+
+function hannoProcess(start, end, from, to, other) {
+  if (end - start <= 1) {
+    console.log(start, from, to);
+    return;
+  }
+  hannoProcess(start, end - 1, from, other, to);
+  hannoProcess(end - 1, end, from, to, other);
+  hannoProcess(start, end - 1, other, to, from);
+}
+
+// hanno(3);
+
+/**
+ * 题目3：打印一个字符串的所有字集，包括空集
+ */
+function subSet(str) {
+  subSetProcess(str.split(''), 0, []);
+}
+
+function subSetProcess(strArray, i, res) {
+  if (i === strArray.length) {
+    console.log(res);
+    return;
+  }
+  let resA = new Array(...res);
+  resA.push(strArray[i]);
+  subSetProcess(strArray, i + 1, resA);
+  let resB = new Array(...res);
+  subSetProcess(strArray, i + 1, resB);
+}
+
+// subSet('sddsasad');
+
+/**
+ * 题目4：打印一个字符串的全部排列
+ */
+function subArrange(str) {
+  // debugger;
+  strArray = str.split('');
+  subArrangeProcess([], strArray);
+}
+
+let count = 0;
+
+function subArrangeProcess(result, resStrArray) {
+  if (resStrArray.length === 0) {
+    count = count + 1;
+    console.log(result);
+    console.log(count);
+
+    return;
+  }
+  resStrArray.forEach((item, i) => {
+    const tempArray = resStrArray.slice();
+    tempArray.splice(i, 1);
+    const itemResult = new Array(...result);
+    itemResult.push(item);
+    subArrangeProcess(itemResult, tempArray);
+  });
+}
+
+// subArrange('sddsasad');
+
+/**
+ * 题目4变形升级：打印一个字符串的全部排列，并且要求不出现重复的排列
+ * 方法自己的
+ */
+function subSingleArrange(str) {
+  // debugger;
+  strArray = str.split('');
+  subSingleArrangeProcess([], strArray, new Set());
+}
+
+function subSingleArrangeProcess(result, resStrArray, resSet) {
+  if (resStrArray.length === 0) {
+    const resStr = result.join('');
+    if (!resSet.has(resStr)) {
+      console.log(resStr);
+      resSet.add(resStr);
+      count = count + 1;
+      console.log(count);
+      return;
+    }
+    return;
+  }
+  resStrArray.forEach((item, i) => {
+    const tempArray = resStrArray.slice();
+    tempArray.splice(i, 1);
+    const itemResult = new Array(...result);
+    itemResult.push(item);
+    subSingleArrangeProcess(itemResult, tempArray, resSet);
+  });
+}
+
+// subSingleArrange('aab');
+
+/**
+ * 题目4变形升级：打印一个字符串的全部排列，并且要求不出现重复的排列
+ * 方法优化，26个字母，存一个是否处理过某字符的长度为26的数组，处理过用1，没处理用0
+ */
+
+/**
+ * 题目5 一个数组，代表一列纸牌，玩家a,b都绝顶聪明，可以从最左或最右拿纸牌，累积数值和大的赢，并且都会拿最优方式（可以看到每一张的值），求拿完后的结果。
+ * P11 1:20:00 https://www.bilibili.com/video/BV13g41157hK?p=11&vd_source=b89b387b8770a0b69ddab591194bd329
+ */
+function abChoosePoker(arr) {
+  console.log(firstChoose(arr, 0, arr.length - 1));
+}
+
+function firstChoose(arr, l, r) {
+  if (l === r) {
+    return arr[l];
+  }
+  let left = arr[l] + secondChoose(arr, l + 1, r);
+  let right = arr[r] + secondChoose(arr, l, r - 1);
+  return Math.max(left, right);
+}
+
+function secondChoose(arr, l, r) {
+  if (l === r) {
+    return 0;
+  }
+  let firstLeft = firstChoose(arr, l + 1, r);
+  let firstRight = firstChoose(arr, l, r - 1);
+  return Math.min(firstLeft, firstRight);
+}
+
+abChoosePoker([1, 2, 100, 4]);
+
+/**
+ * 题目6 给你一个栈，不能申请额外的数据结构，只能使用递归，逆序这个栈
+ * P11 1:32:00 https://www.bilibili.com/video/BV13g41157hK?p=11&vd_source=b89b387b8770a0b69ddab591194bd329
+ */
